0a4d435ea31ca466ac3b914384123bb16220321f
bigtonito39 <bigtonito@hotmail.com>
2019-09-12 20:24:27 -0400

    Updating files

--- assignments/prototype-refactor.js
+++ assignments/prototype-refactor.js
@@ -7,3 +7,233 @@ Prototype Refactor
 2. Your goal is to refactor all of this code to use ES6 Classes. The console.log() statements should still return what is expected of them.
 
 */
+/*
+  Object oriented design is commonly used in video games.  For this part of the assignment you will be implementing several constructor functions with their correct inheritance hierarchy.
+
+  In this file you will be creating three constructor functions: GameObject, CharacterStats, Humanoid.  
+
+  At the bottom of this file are 3 objects that all end up inheriting from Humanoid.  Use the objects at the bottom of the page to test your constructor functions.
+  
+  Each constructor function has unique properties and methods that are defined in their block comments below:
+*/
+
+/*
+  === GameObject ===
+  * createdAt
+  * name
+  * dimensions (These represent the character's size in the video game)
+  * destroy() // prototype method that returns: `${this.name} was removed from the game.`
+*/
+class GameObject {
+    constructor(characterAttr) {
+        this.createdAt = characterAttr.createdAt;
+        this.name = characterAttr.name;
+        this.dimensions = characterAttr.dimensions;
+    }
+    destroy() {
+        return `${this.name} was removed from the game.`
+    }
+
+}
+
+
+
+/*
+  === CharacterStats ===
+  * healthPoints
+  * takeDamage() // prototype method -> returns the string '<object name> took damage.'
+  * should inherit destroy() from GameObject's prototype
+*/
+class CharacterStats extends GameObject {
+
+    constructor(CharacStats) {
+        super(CharacStats)
+        this.healthPoints = CharacStats.healthPoints;
+    }
+    takeDamage() {
+        return `${this.name} took damage.`
+    }
+}
+
+
+
+
+/*
+  === Humanoid (Having an appearance or character resembling that of a human.) ===
+  * team
+  * weapons
+  * language
+  * greet() // prototype method -> returns the string '<object name> offers a greeting in <object language>.'
+  * should inherit destroy() from GameObject through CharacterStats
+  * should inherit takeDamage() from CharacterStats
+*/
+class Humanoid extends CharacterStats {
+
+    constructor(HumanoidAtrr) {
+        super(HumanoidAtrr)
+        this.team = HumanoidAtrr.team;
+        this.weapons = HumanoidAtrr.weapons;
+        this.language = HumanoidAtrr.language;
+    }
+    greet() {
+        return `${this.name} offers a greeting in ${this.language}.`;
+    }
+}
+
+/*
+ * Inheritance chain: GameObject -> CharacterStats -> Humanoid
+ * Instances of Humanoid should have all of the same properties as CharacterStats and GameObject.
+ * Instances of CharacterStats should have all of the same properties as GameObject.
+ */
+
+// Test you work by un-commenting these 3 objects and the list of console logs below:
+
+
+const mage = new Humanoid({
+    createdAt: new Date(),
+    dimensions: {
+        length: 2,
+        width: 1,
+        height: 1,
+    },
+    healthPoints: 5,
+    name: 'Bruce',
+    team: 'Mage Guild',
+    weapons: [
+        'Staff of Shamalama',
+    ],
+    language: 'Common Tongue',
+});
+
+const swordsman = new Humanoid({
+    createdAt: new Date(),
+    dimensions: {
+        length: 2,
+        width: 2,
+        height: 2,
+    },
+    healthPoints: 15,
+    name: 'Sir Mustachio',
+    team: 'The Round Table',
+    weapons: [
+        'Giant Sword',
+        'Shield',
+    ],
+    language: 'Common Tongue',
+});
+
+const archer = new Humanoid({
+    createdAt: new Date(),
+    dimensions: {
+        length: 1,
+        width: 2,
+        height: 4,
+    },
+    healthPoints: 10,
+    name: 'Lilith',
+    team: 'Forest Kingdom',
+    weapons: [
+        'Bow',
+        'Dagger',
+    ],
+    language: 'Elvish',
+});
+
+console.log(mage.createdAt); // Today's date
+console.log(archer.dimensions); // { length: 1, width: 2, height: 4 }
+console.log(swordsman.healthPoints); // 15
+console.log(mage.name); // Bruce
+console.log(swordsman.team); // The Round Table
+console.log(mage.weapons); // Staff of Shamalama
+console.log(archer.language); // Elvish
+console.log(archer.greet()); // Lilith offers a greeting in Elvish.
+console.log(mage.takeDamage()); // Bruce took damage.
+console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
+
+
+// Stretch task: 
+// * Create Villain and Hero constructor functions that inherit from the Humanoid constructor function. 
+class Villain extends Humanoid {
+    constructor(VillainAtrr) {
+        super(VillainAtrr);
+    }
+    GameOver(weapon) {
+        if (weapon === "homeRun") {
+            return `${this.team} won the game`;
+        } else if (weapon === "hit") {
+            return `${this.team} is close to win the game`
+        } else {
+            return "Yey !!! Phillies lost!"
+        }
+    }
+}
+// * Give the Hero and Villains different methods that could be used to remove health points from objects which could result in destruction if health gets to 0 or drops below 0;
+
+
+
+
+class Hero extends Humanoid {
+
+    constructor(HeroAtrr) {
+        super(HeroAtrr)
+    }
+    GameOver(weapon) {
+        if (weapon === "homeRun") {
+            return `${this.team} won the game!!!!`;
+        } else if (weapon === "hit") {
+            return `${this.team} is close to win the game`
+        } else {
+            return "Ohhh no!!! Mets Lost :("
+        }
+    }
+}
+
+
+
+// * Create two new objects, one a villain and one a hero and fight it out with methods!
+
+const MrMets = new Hero({
+    createdAt: new Date(),
+    dimensions: {
+        length: 5,
+        width: 5,
+        height: 3,
+    },
+    healthPoints: 50,
+    name: 'Mr Mets',
+    team: 'Mets',
+    weapons: [
+        'homeRun',
+        'hit',
+        'walk',
+        'strikesOut'
+    ],
+    language: 'New York\'s language',
+});
+
+const PhilliesP = new Villain({
+    createdAt: new Date(),
+    dimensions: {
+        length: 5,
+        width: 5,
+        height: 3,
+    },
+    healthPoints: 50,
+    name: 'Phillie phanatic',
+    team: 'Phillies',
+    weapons: [
+        'homeRun',
+        'hit',
+        'walk',
+        'strikesOut'
+
+    ],
+    language: 'Philadelphia`s Language',
+});
+
+
+console.log(MrMets.greet())
+console.log(PhilliesP.greet())
+console.log(PhilliesP.GameOver("hit"));
+console.log(PhilliesP.GameOver("strikesOut"));
+console.log(MrMets.GameOver("homeRun"));
